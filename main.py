1479967b44e737443c2aeae856506dab2ff5e3dd
acharyafagun-ux <acharyafagun@gmail.com>
2026-02-25 15:31:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-# Add src folder to system path
 sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
 from preprocessing import load_data, clean_data, create_features, transform_and_scale
@@ -14,7 +13,6 @@ import pandas as pd
 
 def main():
 
-    # -------- Paths --------
     raw_data_path = "data/raw/online_retail.csv"
 
     processed_data_path = "data/processed"
@@ -22,35 +20,30 @@ def main():
     cluster_plots_path = "results/cluster_plots"
     pca_outputs_path = "results/pca_outputs"
 
-    # Create folders if not exist
     os.makedirs(processed_data_path, exist_ok=True)
     os.makedirs(results_metrics_path, exist_ok=True)
     os.makedirs(cluster_plots_path, exist_ok=True)
     os.makedirs(pca_outputs_path, exist_ok=True)
 
-    # -------- Load & Clean --------
     df = load_data(raw_data_path)
     df = clean_data(df)
 
-    # Save cleaned dataset
     df.to_csv(os.path.join(processed_data_path, "cleaned_data.csv"), index=False)
 
-    # -------- Feature Engineering --------
+    # Feature Engineering 
     rfm = create_features(df)
 
-    # Save feature-engineered dataset
     rfm.to_csv(os.path.join(processed_data_path, "rfm_features.csv"))
 
-    # Save correlation heatmap
     plot_correlation_heatmap(
         rfm,
         save_path=os.path.join(cluster_plots_path, "correlation_heatmap.png")
     )
 
-    # -------- Transform & Scale --------
+    # Transform & Scale 
     rfm_scaled, rfm_log = transform_and_scale(rfm)
 
-    # -------- Elbow Method --------
+    # Elbow Method 
     inertia = []
     K = range(1, 11)
 
@@ -65,13 +58,13 @@ def main():
         save_path=os.path.join(cluster_plots_path, "elbow_plot.png")
     )
 
-    # -------- Clustering --------
+    # Clustering 
     kmeans_model, kmeans_labels = run_kmeans(rfm_scaled, n_clusters=3)
     hier_model, hier_labels = run_hierarchical(rfm_scaled, n_clusters=3)
     dbscan_model, dbscan_labels = run_dbscan(rfm_scaled)
     gmm_model, gmm_labels = run_gmm(rfm_scaled, n_clusters=3)
 
-    # -------- Evaluation --------
+    # Evaluation 
     results = []
 
     for name, labels in [
@@ -93,7 +86,7 @@ def main():
         index=False
     )
 
-    # -------- Final KMeans Output --------
+    # Final KMeans Output 
     rfm["Cluster"] = kmeans_labels
 
     # Save clustered customer dataset
@@ -101,7 +94,7 @@ def main():
         os.path.join(results_metrics_path, "final_clustered_customers.csv")
     )
 
-    # -------- PCA (Final Visualization) --------
+    #  PCA (Final Visualization) 
     variance_ratio = plot_pca(
         rfm_scaled,
         kmeans_labels,
